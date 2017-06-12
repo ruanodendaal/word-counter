a6f7452a029724d886d629247522a1ea7be8c518
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-12 21:14:25 +0100

    WIP for rendering results on the frontend

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -5,11 +5,15 @@ $(document).ready(function() {
     var reader = new FileReader();
     reader.addEventListener('load', function() {
       fileText = this.result;
-      textProcessor(fileText)
+      processText(fileText)
       document.getElementById('fileDisplayArea').innerText = this.result;
     });
     reader.readAsText(document.querySelector('input').files[0]);
   });
 
+  function processText(fileText) {
+    var wordCounter = new WordCounter(fileText);
+    var bookProcessor = new BookProcessor(wordCounter);
 
+  }
 });
