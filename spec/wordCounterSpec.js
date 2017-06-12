539f27c63cf0ac043b87ffcb7487f7988df0e647
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-12 12:13:31 +0100

    pass removeUnwantedChars test

--- spec/wordCounterSpec.js
+++ spec/wordCounterSpec.js
@@ -5,7 +5,8 @@ describe('WordCounter', function() {
   var str;
 
   beforeEach(function() {
-    wordCounter = new WordCounter();
+    var str = "They needed: pretty# Clothes, good fires, and_a_Mother Goose wall-paper!";
+    wordCounter = new WordCounter(str);
   })
 
   it('initializes with an empty countMap hash', function() {
@@ -17,13 +18,10 @@ describe('WordCounter', function() {
   });
 
   describe('Formatting text', function() {
-    beforeEach(function() {
-      var str = "They needed: pretty# Clothes, good fires, and_a_Mother Goose wall-paper!";
-    });
 
     it('should remove unwanted punctuation', function() {
       var convertedStr = "they needed pretty clothes good fires and a mother goose wall paper ";
-      expect(wordCounter.removeUnwantedChars(str)).toEqual(convertedStr);
+      expect(wordCounter.removeUnwantedChars()).toEqual(convertedStr);
     });
   });
 
--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,6 +1,13 @@
 'use strict';
 
-function WordCounter() {
+function WordCounter(str) {
   this.countMap = {};
   this.wordList = [];
+  this._str = str;
 }
+
+// remove unwanted characters including uppercase letters
+WordCounter.prototype.removeUnwantedChars = function() {
+  this._str = this._str.replace(/[\W_]+|\s+/gm, ' ').toLowerCase();
+  return this._str;
+};
