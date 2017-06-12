120336df813fffd412959aec07b1ad4769cf568f
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-12 13:24:54 +0100

    pass wordFreq test

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -8,6 +8,20 @@ function WordCounter(str) {
 
 // remove unwanted characters including uppercase letters
 WordCounter.prototype.removeUnwantedChars = function() {
-  this._str = this._str.replace(/[\W_]+|\s+/gm, ' ').toLowerCase();
+  this._str = this._str.replace(/[\W_]+/gi, ' ').trim().toLowerCase();
   return this._str;
+}
+
+// iterate through and count each word
+WordCounter.prototype.wordFreq = function() {
+  var words = this._str.split(/\s/);
+  var countMap = {};
+  words.forEach(function(w) {
+    if (!countMap[w]) {
+      countMap[w] = 0;
+    }
+    countMap[w] ++;
+  });
+
+  return countMap;
 };
