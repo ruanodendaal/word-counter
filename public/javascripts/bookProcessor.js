71c5e0bccb40cbcf9db656096d50660cd81721c3
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-12 20:17:45 +0100

    pass runProcessing test

--- public/javascripts/bookProcessor.js
+++ public/javascripts/bookProcessor.js
@@ -1,5 +1,15 @@
 'use strict';
 
-function BookProcessor(wordProcessor) {
-  this.wordProcessor = wordProcessor;
+/* takes in a wordCounter object and prepares the text ready
+  to output on the frontend */
+function BookProcessor(wordCounter) {
+  this.wordProcessor = wordCounter;
+}
+// process all steps for a given file
+BookProcessor.prototype.runProcessing = function() {
+  this.wordProcessor.removeUnwantedChars();
+  this.wordProcessor.wordFreq();
+  this.wordProcessor.mapPrimeNumbers()
+
+  return this.dataObj = this.wordProcessor.countMap
 }
