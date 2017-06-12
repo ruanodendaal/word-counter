98b5a2b2e222f91539e4fcb1c0b0f99d21d56d9e
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-12 11:58:14 +0100

    failing removeUnwantedChars test

--- spec/wordCounterSpec.js
+++ spec/wordCounterSpec.js
@@ -2,6 +2,7 @@
 
 describe('WordCounter', function() {
   var wordCounter;
+  var str;
 
   beforeEach(function() {
     wordCounter = new WordCounter();
@@ -13,6 +14,17 @@ describe('WordCounter', function() {
 
   it('initializes with an empty wordList array', function() {
     expect(wordCounter.wordList).toEqual([]);
+  });
+
+  describe('Formatting text', function() {
+    beforeEach(function() {
+      var str = "They needed: pretty# Clothes, good fires, and_a_Mother Goose wall-paper!";
+    });
+
+    it('should remove unwanted punctuation', function() {
+      var convertedStr = "they needed pretty clothes good fires and a mother goose wall paper ";
+      expect(wordCounter.removeUnwantedChars(str)).toEqual(convertedStr);
+    });
   });
 
 });
