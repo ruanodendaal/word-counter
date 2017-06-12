ea529c49ae0169ad237d386e6b450f34895d704a
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-12 12:32:24 +0100

    failing wordFreq test

--- spec/wordCounterSpec.js
+++ spec/wordCounterSpec.js
@@ -20,9 +20,20 @@ describe('WordCounter', function() {
   describe('Formatting text', function() {
 
     it('should remove unwanted punctuation', function() {
-      var convertedStr = "they needed pretty clothes good fires and a mother goose wall paper ";
+      var convertedStr = "they needed pretty clothes good fires and a mother goose wall paper";
       expect(wordCounter.removeUnwantedChars()).toEqual(convertedStr);
     });
   });
 
+  describe('WordCounter', function() {
+    it('counts how many times each word appears', function() {
+      var testStr = "They, they he clothes and fires and mother goose goose";
+      wordCounter = new WordCounter(testStr);
+      wordCounter.removeUnwantedChars();
+      expect(wordCounter.wordFreq()).toEqual({ they: 2, and: 2,
+                                               he: 1, clothes: 1,
+                                               fires: 1, mother: 1,
+                                               goose: 2});
+    });
+  });
 });
